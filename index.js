47de9682aa36d1a800278a194ff6561ac0f3e8e7
ramanath-naik <108948463+ramanath-naik@users.noreply.github.com>
2024-03-19 11:06:54 +0530

    roll the dice app

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import AppPro from './AppPro';
+import AppPro from './src/AppPro';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => AppPro);
